e07e1538d324f54a542addf729ea81a8ead0679a
Cam Utz <Dragon8029@gmail.com>
2019-04-27 06:44:05 -0600

    add session middleware

--- myapp/app.js
+++ myapp/app.js
@@ -23,6 +23,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+app.use(session({ secret: 'session secret key' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
