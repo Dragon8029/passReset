1cdb86facd6d98e89a83b65a2eddbfa1ab0e8246
Cam Utz <Dragon8029@gmail.com>
2019-05-03 18:52:09 -0700

    add dependencies

--- passwordreset/app.js
+++ passwordreset/app.js
@@ -1,11 +1,11 @@
-var jwt = require('jwt-simple');
+const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jwt-simple');
+const app = express();
 
-var payload = { userId: 1 };
-var secret = 'fe1a1915a379f3be5394b64d14794932';
-var token = jwt.encode(payload, secret);
+app.use(bodyParser.urlencoded({ extended: false }));
 
-console.log(token);
 
-var decode = jwt.decode(token, secret);
-
-console.log(decode);
+app.listen(3000, function() {
+    console.log('Node started on port 3000!')
+});
