9845812282fc4da6419bfeb979dc23554c0992c8
Cam Utz <Dragon8029@gmail.com>
2019-05-12 06:14:57 -0700

    remove unused code

--- myapp/app.js
+++ myapp/app.js
@@ -257,18 +257,7 @@ app.post('/reset/:token', function(req, res) {
   });
 });
 
-// using SendGrid's v3 Node.js Library
-// https://github.com/sendgrid/sendgrid-nodejs
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const msg = {
-  to: 'test@example.com',
-  from: 'test@example.com',
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-};
-sgMail.send(msg);
+
 
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
